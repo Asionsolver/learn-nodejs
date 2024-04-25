a445893fb04faafd67bd7cb93c1ee9d656d1bee4
asionsolver <asionsolver274@gmail.com>
2024-04-25 12:48:14 +0600

    Learn nodejs modules name like http modules

--- http/http.js
+++ http/http.js
@@ -1,7 +1,23 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello Node.js World!");
+    res.write("How are you?");
+    res.end();
+  }else if (req.url === "/about") {
+    res.write("This is the about page");
+    res.end();
+  }else{
+    res.write("Not found");
+    res.end();
+  }
+});
+
+// server.on('connection', () => {
+//     console.log('New connection...');
+// });
 
 server.listen(3000);
 
-console.log('Listening on port 3000...');
+console.log("Listening on port 3000...");
