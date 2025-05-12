5a999106fe2f68cbc0d59d8be85018715e4a8b1d
Asionsolver <asionsolver274@gmail.com>
2025-05-12 12:06:08 +0600

    clear all file

--- event/extendEvents.js
+++
@@ -1,7 +0,0 @@
-const School = require('./school');
-const school = new School();
-
-school.on('bellRing', ({ period, text }) => {
-    console.log(`Period ${period}${text}. Time to go to the next class!`);
-})
-school.startPeriod();
